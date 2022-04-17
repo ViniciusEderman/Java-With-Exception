24d0634e2c4269cfe9d05daacbc1433e2b10d7b8
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-04-17 16:19:06 -0300

    acrescentando condição de encerramento do programa

--- JavaCursoDio/PrimeiraAula.java
+++ JavaCursoDio/PrimeiraAula.java
@@ -8,18 +8,24 @@ public class PrimeiraAula {
     do {
         String a = JOptionPane.showInputDialog("Numerador: ");
         String b = JOptionPane.showInputDialog("Denominador: ");
-        try{
-          int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
-          System.out.println("Resultado: " + resultado);
+        if(a != null && b != null) {
+          try{
+            int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+            System.out.println("Resultado: " + resultado);
+            continueLooping = false;
+          }catch(NumberFormatException e) {
+              JOptionPane.showMessageDialog(null, "Entrada inválida, utilize apenas números.");
+              // e.printStackTrace();
+          }catch(ArithmeticException e) {
+              JOptionPane.showMessageDialog(null, "Entrada inválida, não possível dividir por 0");
+          }finally {
+              System.out.println("Sistema finalizado!");
+          }
+        }else{
           continueLooping = false;
-        }catch(NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Entrada inválida, utilize apenas números.");
-            // e.printStackTrace();
-        }catch(ArithmeticException e) {
-            JOptionPane.showMessageDialog(null, "Entrada inválida, não possível dividr por 0");
-        }finally {
-            System.out.println("Sistema finalizado!");
-        }
+          System.out.println("O valor está vázio, acrescente o numerador e denominador inteiros.");
+          System.out.println("Encerrando o programa...");
+        }    
     }while(continueLooping);
   }
     public static int dividir(int a, int b) { return a / b; }
