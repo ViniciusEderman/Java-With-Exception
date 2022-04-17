efcec205453189221c02b1d7de115be25d1acb8a
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-04-17 09:38:40 -0300

    Update PrimeiraAula.java

--- JavaCursoDio/PrimeiraAula.java
+++ JavaCursoDio/PrimeiraAula.java
@@ -4,17 +4,24 @@ import javax.swing.JOptionPane;
 
 public class PrimeiraAula {
   public static void main(String [] args) {
-    String a = JOptionPane.showInputDialog("Numerador: ");
-    String b = JOptionPane.showInputDialog("Denominador: ");
-    try{
-      int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
-      System.out.println("Resultado: " + resultado);
-    }catch(NumberFormatException e) {
-        JOptionPane.showMessageDialog(null, "Entrada inválida, utilize apenas números.");
-        // e.printStackTrace();
-    }finally{
-        System.out.println("Sistema finalizado!");
-    }
+    boolean continueLooping = true;
+    do {
+        String a = JOptionPane.showInputDialog("Numerador: ");
+        String b = JOptionPane.showInputDialog("Denominador: ");
+        try{
+          int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+          System.out.println("Resultado: " + resultado);
+          continueLooping = false;
+        }catch(NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Entrada inválida, utilize apenas números.");
+            // e.printStackTrace();
+        }catch(ArithmeticException e) {
+            JOptionPane.showMessageDialog(null, "Entrada inválida, não possível dividr por 0");
+        }finally {
+            System.out.println("Sistema finalizado!");
+        }
+    }while(continueLooping);
   }
     public static int dividir(int a, int b) { return a / b; }
 }
+ 
