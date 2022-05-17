6794063ad11d831332b0511e6c05dc2be321bbee
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-05-17 20:57:39 -0300

    Apenas uma questão de lógica

---
+++ javaMonitoria/whileExemple.java
@@ -0,0 +1,22 @@
+package javaMonitoria;
+import java.util.Scanner;
+
+public class whileExemple {
+    public static void main(String [] args) {
+        /*Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores "M" e "F".
+        Caso digitado qualquer coisa que não seja m/f retormar para que o usuário digite novamente. */
+
+        Scanner keyboard = new Scanner(System.in);
+        String sex = "";
+
+         do {
+           System.out.println("Digite o seu sexo, apenas M para homem e F para mulher: ");
+           sex = keyboard.next();
+           System.out.println(sex.equals("F"));
+           System.out.println(sex.equals("M"));
+         }
+         while((sex != "F") && (sex != "M"));
+            
+        keyboard.close();    
+    }
+}   
