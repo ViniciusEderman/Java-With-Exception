86287e82cdc0585c83a6c8ffbc9925d7435262ca
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-05-18 11:07:07 -0300

    3 exemplos

---
+++ javaMonitoria/doWhile.java
@@ -0,0 +1,42 @@
+//neste exemplo irei mostrar o que acontece usando while e do while para a mesma solução
+
+/* package javaMonitoria;
+
+public class doWhile {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        while(contador <= 10) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+} */
+
+/* package javaMonitoria;
+
+public class doWhile {
+    public static void main(String[] args) {
+        int contador = 15;
+
+        while(contador <= 10) {
+            System.out.println(contador);
+            contador++;
+        }
+        System.out.println("Testeeeeeeeeeeee " + contador);
+    }
+} */
+
+/*package javaMonitoria;
+
+public class doWhile {
+    public static void main(String[] args) {
+        int contador = 15;
+
+        do {
+            contador = contador + 5;
+            System.out.println(contador);
+            
+        } while(contador <= 10);
+    }
+} */
