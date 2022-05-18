a2e5f4da52db69cb344da3320bd79d475cf88ce1
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-05-17 21:09:09 -0300

    javaMonitoria/whileExemple.java

--- javaMonitoria/whileExemple.java
+++ javaMonitoria/whileExemple.java
@@ -1,10 +1,10 @@
-package javaMonitoria;
+/*package javaMonitoria;
 import java.util.Scanner;
 
 public class whileExemple {
     public static void main(String [] args) {
         /*Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores "M" e "F".
-        Caso digitado qualquer coisa que não seja m/f retormar para que o usuário digite novamente. */
+        Caso digitado qualquer coisa que não seja m/f retormar para que o usuário digite novamente. 
 
         Scanner keyboard = new Scanner(System.in);
         String sex = "";
@@ -19,4 +19,27 @@ public class whileExemple {
             
         keyboard.close();    
     }
-}   
+}   */
+
+
+package javaMonitoria;
+import java.util.Scanner;
+
+public class whileExemple {
+    public static void main(String [] args) {
+        /*Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores "M" e "F".
+        Caso digitado qualquer coisa que não seja m/f retormar para que o usuário digite novamente. */
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Digite o seu sexo, apenas M para homem e F para mulher: ");
+        String sex = keyboard.nextLine();
+       
+            while(sex != "F" ||sex != "M") {
+                System.out.println("Digite o seu sexo, apenas M para homem e F para mulher: ");
+                sex = keyboard.nextLine();
+            }
+            System.out.println("O seu sexo foi registrado com sucesso");
+        keyboard.close();    
+    }
+
+}
